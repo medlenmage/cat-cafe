fbb1e631c18f30b008e6deaa202998e06c38901c
Joshua Medlen <joshua246@gmail.com>
2020-08-22 13:51:40 -0500

    working on authentication

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import fbConnections from '../helpers/data/connection';
 import './App.scss';
+import Auth from '../components/Auth/Auth';
+
+fbConnections();
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <Auth />
       </div>
     );
   }
